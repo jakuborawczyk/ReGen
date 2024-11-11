2d33682a59451f7d4abde906852304f5b8546322
Hampus9 <hampus.asadour@gmail.com>
2024-11-12 00:03:04 +0100

    added sortiment-selection-items and the our story grid, it's responsive but will have to fix the spacing.

--- index.php
+++ index.php
@@ -2,35 +2,84 @@
 
 declare(strict_types=1);
 
-require __DIR__.'/footer.php';?>
+require __DIR__ . '/footer.php'; ?>
 
-    <div class="hero"> <!--utan grid på mobilt-->
-      <!-- Jakub bygger -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <div class="hero"> <!--utan grid på mobilt-->
+    <!-- Jakub bygger -->
+  </div>
+  <div class="sortiment">
+    <!-- Jakub bygger -->
+  </div>
+  <form>
+    <!-- Jakub bygger -->
+  </form>
+  <div class="hero-inspiration">
+
+    <!-- Hampus bygger -->
+    <!--om oss och sustainablity är här i mobilt -->
+
+    <!-- sortiment selection items-->
+  </div>
+  <div class="sortiment-selected-items">
+    <div class="sortiment-selection-wrap">
+
+      <div class="sortiment-selection-wraper">
+        <h3>News/seasonality</h3>
+        <div class="sortiment-selection-1"></div>
+      </div>
+
+      <div class="sortiment-selection-wraper">
+        <h3>Our most beloved</h3>
+        <div class="sortiment-selection-2"></div>
+      </div>
+
+      <div class="sortiment-selection-hidden">
+        <div class="sortiment-selection-wraper">
+          <h3>Om oss</h3>
+          <div class="sortiment-selsction-3"></div>
+        </div>
+
+        <div class="sortiment-selection-wraper">
+          <h3>Hållbarhet</h3>
+          <div class="sortiment-selection-4"></div>
+        </div>
+      </div>
     </div>
-    <div class="sortiment">
-      <!-- Jakub bygger -->
+  </div>
+  <!--sortiment selection items end-->
+
+  <!--our story start-->
+  <div class="our-story-centered">
+    <div class="our-story">
+      <div class="our-story-box-1"></div>
+      <div class="our-story-box-2"></div>
+      <div class="our-story-box-3"></div>
+      <div class="our-story-box-4"></div>
+      <div class="our-story-box-5"></div>
+      <div class="our-story-box-6"></div>
     </div>
-    <form>
-      <!-- Jakub bygger -->
-    </form>
-    <div class="hero-inspiration">
-      <!-- Hampus bygger -->
-    </div>
-    <div class="sortiment-selected-items"> <!--om oss och sustainablity är här i mobilt -->
-      <!-- Hampus bygger -->
-    </div>
-    <div class="our-story"> <!--bara desktop-->
-      <h1>hampus byggerh1</h1>
-      <!-- Hampus bygger -->
-    </div>
-    <div class="sustainability"> <!--bara desktop-->
-      <!-- Andrea bygger -->
-    </div>
-    "
-    <div class="social-media">
-      <!-- Andrea Bygger -->
+  </div>
+  <!--our story end-->
+
+  <div class="sustainability"> <!--bara desktop-->
+    <!-- Andrea bygger -->
+  </div>
+  <div class="social-media">
+    <!-- Andrea Bygger -->
 
 
-    </div>
+  </div>
 
-    <?php require __DIR__.'/footer.php';
+  <?php require __DIR__ . '/footer.php'; ?>
