2ec545506706f7dcfabc026b0e57eef65ba0a887
jakuborawczyk <jakuboraw@gmail.com>
2024-11-12 19:04:12 +0100

    started to make nav and fixec scroll that was horistontally

--- header.php
+++ header.php
@@ -1,16 +1,36 @@
 <?php
 
-declare(strict_types=1);?>
+declare(strict_types=1); ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <body>
-    <header>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+  <title>Document</title>
+</head>
+
+<body>
+  <header>
+    <div class="logoHeader">
+      <a href="#">Sortiment</a>
+      <a class="desktop-only" href="#">Om oss</a>
+      <a class="desktop-only" href="#">Mer →</a>
+    </div>
+    <nav>
+      <div class="navContainer"><i class="fa fa-shopping-bag fa-xl"></i> <span class="desktop-only navText">Shopping</span></div> 
+      <div class="navContainer"><i class="fa fa-heart fa-xl"></i> <span class="desktop-only navText">Favoriter</span></div> 
+      <div class="navContainer"><i class="fa fa-user fa-xl"></i> <span class="desktop-only navText">Konto</span></div> 
+      <div class="navContainer"><i class="fa fa-globe fa-xl"></i> <span class="desktop-only navText">Sv</span></div> 
+      <div class="navContainer"><i class="fa fa-search fa-xl"></i> <span class="desktop-only navText">Sök</span></div> 
+    </nav>
       <!-- Jakub bygger -->
-    </header>
+
+  </header>
