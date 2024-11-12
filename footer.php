76a96456eafadc9aeadf5616f54254d68f521825
Andreawingardh <andreawingardh@gmail.com>
2024-11-12 15:03:26 +0100

    Added divs to footer

--- footer.php
+++ footer.php
@@ -4,6 +4,12 @@ declare(strict_types=1);?>
 
 <footer>
 <!-- Andrea bygger -->
+ <div class ="footer-logo"></div>
+ <div class="footer-follow-us"></div>
+ <hr class ="footer">
+ <div class ="footer-general-information"></div>
+ <div class="footer-integrity-cookie"></div>
+ <div class="footer-payment-information"></div>
 </footer>
 </body>
 </html>
