e7104edf213d898fd107f8f88e9769c2c88e5351
Andreawingardh <andreawingardh@gmail.com>
2024-11-13 15:18:10 +0100

    Added divs to footer.php

--- footer.php
+++ footer.php
@@ -4,12 +4,30 @@ declare(strict_types=1);?>
 
 <footer>
 <!-- Andrea bygger -->
- <div class ="footer-logo"></div>
- <div class="footer-follow-us"></div>
+ <div class ="footer-logo">Logo</div>
+ <div class="footer-follow-us">
+    <div class="footer-some">x</div>
+    <div class="footer-some">x</div>
+    <div class="footer-some">x</div>
+    <div class="footer-some">x</div>
+    <div class="footer-some">x</div>
+ </div>
  <hr class ="footer">
- <div class ="footer-general-information"></div>
- <div class="footer-integrity-cookie"></div>
- <div class="footer-payment-information"></div>
+ <div class ="footer-general-information">
+    <div class="footer-general-text">Sortiment</div>
+    <div class="footer-general-text">Om Oss</div>
+    <div class="footer-general-text">Kontakt</div>
+    <div class="footer-general-text">Q&A</div>
+    <div class="footer-general-text">Support</div>
+ </div>
+ <!-- <div class="footer-integrity-cookie"></div> -->
+ <div class="footer-payment-information">
+    <div class="footer-ecommerce"></div>
+    <div class="footer-ecommerce"></div>
+    <div class="footer-ecommerce"></div>
+    <div class="footer-text">Integritetspolicy</div>
+    <div class="footer-text">Cookie policy</div>
+ </div>
 </footer>
 </body>
 </html>
