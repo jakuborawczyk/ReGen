18f19df400916ae0f21433955c5cd0fb07e2cbd2
Jakub Orawczyk <jakuboraw@gmail.com>
2024-11-17 17:43:20 +0100

    removed the whole navbar, it wouldnt cooperate with me so im going to start build it from scratch. Now im going to do the part underneath header

--- header.php
+++ header.php
@@ -12,28 +12,11 @@ declare(strict_types=1); ?>
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
   <title>Document</title>
 </head>
 
 <body>
-  <header>
-    <div class="logoHeader">
-      <img class="logoIMG" src="/../assets/Smallregular.png" alt="">
-      <div>
-      <a class="headerText desktop-only" href="#">Om oss</a>
-      <a class="headerText" href="#">Sortiment</a>
-      <a class="desktop-only headerText" href="#">Mer→</a>
-    </div>
-    </div>
-    <nav>
-      <div class="navContainer"><i class="fa fa-shopping-bag iconResize iconContainer"></i> <span class="desktop-only navText">Shopping</span></div> 
-      <div class="navContainer"><i class="fa fa-heart iconResize   iconContainer"></i> <span class="desktop-only navText">Favoriter</span></div> 
-      <div class="navContainer"><i class="fa fa-user iconResize  iconContainer"></i> <span class="desktop-only navText">Konto</span></div> 
-      <div class="navContainer"><i class="fa fa-globe   iconResize   iconContainer"></i> <span class="desktop-only navText">Sv</span></div> 
-      <div class="navContainer"><i class="fa fa-search  iconResize   iconContainer"></i> <span class="desktop-only navText">Sök</span></div> 
-    </nav>
-      <!-- Jakub bygger -->
-
-  </header>
+    <!-- Jakub bygger -->
+    <header></header>
