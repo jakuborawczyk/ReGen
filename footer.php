4a4f3293bebe6bdde4fc22c523618725a92dca46
Andreawingardh <andreawingardh@gmail.com>
2024-11-19 09:13:14 +0100

    Added images to footer-payment-information-desktop

--- footer.php
+++ footer.php
@@ -12,6 +12,11 @@ declare(strict_types=1);?>
     <div class="footer-some">x</div>
     <div class="footer-some">x</div>
  </div>
+ <div class="footer-payment-information-desktop">
+   <img class="footer-payment-icon-desktop" src="assets/Trygg E-handel-desktop.png" />
+   <img class="footer-payment-icon-desktop" src="assets/Klarna-desktop.png" />
+   <img class="footer-payment-icon-desktop" src="assets/Swish-desktop.png"/>
+ </div>
  <hr class ="footer">
  <div class ="footer-general-information">
     <div class="footer-general-text">Sortiment</div>
@@ -21,9 +26,10 @@ declare(strict_types=1);?>
     <div class="footer-general-text">Support</div>
  </div>
  <!-- <div class="footer-integrity-cookie"></div> -->
+ <div class="flex-break"></div>
  <div class="footer-payment-information">
     <div class="footer-ecommerce-mobile"></div>
-    <div class="footer-ecommerce-mobild"></div>
+    <div class="footer-ecommerce-mobile"></div>
     <div class="footer-ecommerce-mobile"></div>
     <div class="footer-text">Integritetspolicy</div>
     <div class="footer-text">Cookie policy</div>
