ee54081c35538160f1fc2e5b83ca9df6fe015f25
Andreawingardh <andreawingardh@gmail.com>
2024-11-22 16:50:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function changeColor() {
+    const colorButton = document.getElementById("configurator-jewelry");
+    colorButton.classList.toggle("silver");
+    colorButton.classList.toggle("gold");
+    const colorButtonSilver = document.getElementById("silver-text");
+    colorButtonSilver.classList.toggle("bold");
+    const colorButtonGold = document.getElementById("gold-text");
+    colorButtonGold.classList.toggle("bold");
 
+
+  }
