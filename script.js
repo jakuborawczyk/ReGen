a9ce1ca0769d85717a236097a8dd9065b35b9238
Andreawingardh <andreawingardh@gmail.com>
2024-11-25 17:32:03 +0100

    Added two variations of function changeColor()

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-function changeColor() {
+function changeColorSilver() {
     const colorButton = document.getElementById("configurator-jewelry");
-    colorButton.classList.toggle("silver");
-    colorButton.classList.toggle("gold");
+    colorButton.classList.add("silver");
+    colorButton.classList.remove("gold");
     const colorButtonSilver = document.getElementById("silver-text");
-    colorButtonSilver.classList.toggle("bold");
+    colorButtonSilver.style.fontWeight = '800';
     const colorButtonGold = document.getElementById("gold-text");
-    colorButtonGold.classList.toggle("bold");
+    colorButtonGold.style.fontWeight = '400';
+
+
+  }
+
+  function changeColorGold() {
+    const colorButton = document.getElementById("configurator-jewelry");
+    colorButton.classList.remove("silver");
+    colorButton.classList.add("gold");
+    const colorButtonSilver = document.getElementById("silver-text");
+    colorButtonSilver.style.fontWeight = '400';
+    const colorButtonGold = document.getElementById("gold-text");
+    colorButtonGold.style.fontWeight = '800';
 
 
   }
